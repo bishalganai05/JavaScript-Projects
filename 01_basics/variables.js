3f6e3ef613cbf805876f4fd2846c012c5fcbea9c
Bishal Ganai <113580047+bishalganai05@users.noreply.github.com>
2024-02-15 04:31:44 +0000

    variables basics

---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const name1 = "Bishal";
+let name2 = "Amit";
+var name3 = "Ajay";
+name4 = "Sanjay";
+let name5;
+
+console.table([name1,name2,name3,name4,name5]);
+
+//name1 = "Anil";
+name2 = "Sushant";
+name3 = "Abhay";
+name4 = "Abhraham";
+name5 = "Asim";
+
+console.table([name1,name2,name3,name4,name5]);
